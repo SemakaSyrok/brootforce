45a2a67781df2542183823d55b1eff9e02e9f613
SemakaSyrok <semakasyrok1@gmail.com>
2020-01-13 14:51:17 +0300

    first commit

--- main.go
+++ main.go
@@ -8,9 +8,11 @@ import (
 	"os"
 )
 
+//тут словарь по которому будет подбираться пароль рекурсивно
 var library = []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "g", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "#", "$"}
 var test = []string{"v", "r", "#", "a"}
 
+//сначала вызывается эта функция
 func main() {
 
 	for i := 0; i <= 10; i++ {
@@ -36,8 +38,9 @@ func sendRequest(password string) {
 		"password": {password},
 	}
 
-	// fmt.Println(password)
+	fmt.Println(password)
 
+	//Поменяйте значение на свой сайт
 	resp, err := http.PostForm("http://password/", formData)
 	if err != nil {
 		fmt.Println("Error")
